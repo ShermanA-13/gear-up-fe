a7a3bae1fd140c99b2329ab49fec76fcd527a5bd
Alicia Watt <alicia.watt.aw@gmail.com>
2022-06-03 09:56:19 -0600

    test: add start_date and end_date to poros trip test

--- spec/poros/trips_spec.rb
+++ spec/poros/trips_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "trips poro" do
   let!(:attributes_user) { {id: 1, type: "user", attributes: {name: "Bob", email: "email@gmail.com"}} }
   let!(:user) { User.new(attributes_user) }
-  let!(:attributes) { {id: 1, type: "trip", attributes: {name: "Steve's Crag", location: "some location", host_id: user.id}} }
+  let!(:attributes) { {id: 1, type: "trip", attributes: {name: "Steve's Crag", location: "some location", start_date: "2022-07-10", end_date: "2022-07-20", host_id: user.id}} }
   let!(:trip) { Trip.new(attributes) }
 
   it "exists" do
@@ -13,8 +13,8 @@ RSpec.describe "trips poro" do
   it "has attributes" do
     expect(trip.name).to eq("Steve's Crag")
     expect(trip.location).to eq("some location")
-    # expect(trip.start_date).to eq()
-    # expect(trip.end_date).to eq()
+    expect(trip.start_date).to eq("2022-07-10")
+    expect(trip.end_date).to eq("2022-07-20")
     expect(trip.id).to eq(1)
     expect(trip.host_id).to eq(1)
   end
