21311073918054edf9baa43e26a2032ebfaec9b5
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-03 09:55:33 -0600

    Rework spec test test for CircleCI

--- spec/facades/food_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-describe "test test" do
-  it "can tell a string is a string" do
-    str = "test"
-    
-    expect(str).to be_a(String)
-  end
-end
--- spec/nothing_spec.rb
+++ spec/nothing_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 describe "test test" do
   it "can tell a string is a string" do
     str = "test"
-binding.pry
     expect(str).to be_a(String)
   end
 end
