eb3b4ecf8b18c2d6ea061236802137227c559358
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-03 15:49:38 -0600

    Test: refactor user poro to include first_name & last_name attributes

--- spec/poros/user_spec.rb
+++ spec/poros/user_spec.rb
@@ -3,13 +3,20 @@ require "rails_helper"
 RSpec.describe "user poro" do
   it "exists with attributes" do
     data = {id: "1",
-            type: "user",
-            attributes: {email: "email@gmail.com"}}
+        type: "user",
+        attributes: {
+          email: "email@gmail.com",
+          first_name: "Fox",
+          last_name: "McCloud"
+        }
+      }
 
     user = User.new(data)
 
     expect(user).to be_a(User)
     expect(user.id).to eq("1")
     expect(user.email).to eq("email@gmail.com")
+    expect(user.first_name).to eq("Fox")
+    expect(user.last_name).to eq("McCloud")
   end
 end
