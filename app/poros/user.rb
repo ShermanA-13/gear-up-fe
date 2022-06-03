e7eb61631de8472d17b88002b73c94984d7da026
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-03 15:49:47 -0600

    Poro: refactor user poro to include first_name & last_name attributes

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,9 +1,10 @@
 class User
-  attr_reader :id,
-    :email
+  attr_reader :id, :email, :first_name, :last_name
 
   def initialize(data)
     @id = data[:id]
     @email = data[:attributes][:email]
+    @first_name = data[:attributes][:first_name]
+    @last_name = data[:attributes][:last_name]
   end
 end
