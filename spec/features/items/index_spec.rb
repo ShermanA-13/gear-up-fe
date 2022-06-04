0f86617a2f31e7da0435e5a6d8c24d81fa285f76
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-03 18:57:37 -0600

    Test: item index has links to item show pages

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -2,7 +2,12 @@ require "rails_helper"
 
 describe "Item index/shed" do
   before do
+    # VCR.insert_cassette("displays_all_of_the_users_items")
     visit "/users/1/items"
+  end
+
+  after do
+    # VCR.eject_cassette
   end
 
   it "displays all of the users items", :vcr do
@@ -20,5 +25,16 @@ describe "Item index/shed" do
     expect(page).not_to have_content("Good Socks")
     expect(page).not_to have_content("Count: 3")
     expect(page).not_to have_content("Item ID: 3")
+  end
+
+  it "each item links to the item's show page", :vcr do
+    click_link("Water Bottle")
+
+    expect(current_path).to eq("/users/1/items/1")
+    expect(page).to have_content("Name: Water Bottle")
+    expect(page).to have_content("Count: 5")
+
+    expect(page).not_to have_content("Trail Mix")
+    expect(page).not_to have_content("Count: 8")
   end
 end
