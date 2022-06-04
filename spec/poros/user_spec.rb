db89a70a83f9b61edc5de014d2ee90d659d32780
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-03 19:52:55 -0600

    Test: update User poro test to accomodate first/last name

--- spec/poros/user_spec.rb
+++ spec/poros/user_spec.rb
@@ -4,12 +4,19 @@ RSpec.describe "user poro" do
   it "exists with attributes" do
     data = {id: "1",
             type: "user",
-            attributes: {email: "email@gmail.com"}}
+            attributes: {
+              first_name: "User 1",
+              last_name: "Test",
+              email: "email@gmail.com"
+              }
+            }
 
     user = User.new(data)
 
     expect(user).to be_a(User)
     expect(user.id).to eq("1")
     expect(user.email).to eq("email@gmail.com")
+    expect(user.first_name).to eq("User 1")
+    expect(user.last_name).to eq("Test")
   end
 end
