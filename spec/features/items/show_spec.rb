789b438a9f6288a9d40139e8445d30ce1fedf53d
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-03 18:27:49 -0600

    Update tests to use facade/service

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -2,19 +2,10 @@ require "rails_helper"
 
 describe "Item show page" do
   before do
-    user_1_attr = {id: 1, type: "user", attributes: {email: "user_1@gearup.test"}}
-    @user_1 = User.new(user_1_attr)
-
-    item_1_attr = {id: 1, type: "item", attributes: {name: "Item 1", description: "1st Item Description", category: "Tent", user_id: @user_1.id }}
-    @item_1 = Item.new(item_1_attr)
-
-    item_2_attr = {id: 2, type: "item", attributes: {name: "Item 2", description: "2nd Item Description", category: "Food", user_id: @user_1.id }}
-    @item_2 = Item.new(item_2_attr)
-
-    visit "/users/#{@user_1.id}/items/#{@item_1.id}"
+    visit "/users/1/items/1"
   end
 
-  it "displays the item's attributes (name, desc, categ)" do
+  it "displays the item's attributes (name, desc, categ)", :vcr do
     expect(page).to have_content("Item 1")
     expect(page).to have_content("Description: 1st Item Description")
     expect(page).to have_content("Item Category: Tent")
--- spec/poros/item_spec.rb
+++ spec/poros/item_spec.rb
@@ -5,10 +5,10 @@ describe "Item poro" do
     user_1_attr = {id: 1, type: "user", attributes: {email: "user_1@gearup.test"}}
     @user_1 = User.new(user_1_attr)
 
-    item_1_attr = {id: 1, type: "item", attributes: {name: "Item 1", description: "1st Item Description", category: "Tent", user_id: @user_1.id }}
+    item_1_attr = {id: 1, type: "item", attributes: {name: "Item 1", description: "1st Item Description", count: 1, category: "Tent", user_id: @user_1.id }}
     @item_1 = Item.new(item_1_attr)
 
-    item_2_attr = {id: 2, type: "item", attributes: {name: "Item 2", description: "2nd Item Description", category: "Food", user_id: @user_1.id }}
+    item_2_attr = {id: 2, type: "item", attributes: {name: "Item 2", description: "2nd Item Description", count: 1, category: "Food", user_id: @user_1.id }}
     @item_2 = Item.new(item_2_attr)
   end
 
@@ -17,6 +17,7 @@ describe "Item poro" do
     expect(@item_1.id).to eq(1)
     expect(@item_1.name).to eq("Item 1")
     expect(@item_1.description).to eq("1st Item Description")
+    expect(@item_1.count).to eq(1)
     expect(@item_1.category).to eq("Tent")
     expect(@item_1.user_id).to eq(@user_1.id)
 
