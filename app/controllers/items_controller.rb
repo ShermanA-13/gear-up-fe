ca56c13911747f776895b32c4573567da9c87646
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-03 18:26:37 -0600

    Update item controller to use facade

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,5 @@
 class ItemsController < ApplicationController
   def show
-    user_1_attr = {id: 1, type: "user", attributes: {email: "user_1@gearup.test"}}
-    user_1 = User.new(user_1_attr)
-    item_1_attr = {id: 1, type: "item", attributes: {name: "Item 1", description: "1st Item Description", category: "Tent", user_id: user_1.id }}
-    item_1 = Item.new(item_1_attr)
-
-    @item = item_1
+    @item = GearUpFacade.find_item(params[:user_id], params[:id])
   end
 end
