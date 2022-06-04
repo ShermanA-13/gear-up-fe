bf92fbf6ec853438de440d9e372eeaf56a637f39
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-04 01:00:48 -0600

    Facade: user facade creates and returns user poros

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -1,20 +1,14 @@
 class UserFacade
-  def self.create_user(data)
-    json = UserService.create_user(data)
-    User.new(json[:data])
-  end
-
-  def self.users
-    json = UserService.users
-
-    json[:data].map do |merchant_data|
-      User.new(merchant_data)
+  class << self
+    def create_user(data)
+      json = UserService.create_user(data)
+      User.new(json[:data])
     end
-  end
 
-  def self.user(data)
-    json = UserService.user(data)
+    def user(id)
+      json = UserService.user(id)
 
-    User.new(json[:data])
+      User.new(json[:data])
+    end
   end
 end
