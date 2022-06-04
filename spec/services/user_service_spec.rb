581e1659c5085af05d6f5083b17c9e5764ba1d19
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-04 00:50:04 -0600

    Test: user service returns users by id

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe 'user service' do
     expect(response[:data][:attributes]).to have_key :last_name
     expect(response[:data][:attributes]).to have_key :email
   end
+
+  it 'returns users by id' do
+    data = {
+      first_name: 'Pickles',
+      last_name: 'McTickles',
+      email: 'dilllord@dills.org'
+    }
+    user = UserService.create_user(data)
+
+    response = UserService.user(user[:data][:id])
+    expect(response).to be_a Hash
+    expect(response[:data]).to be_a Hash
+    expect(response[:data]).to have_key :id
+    expect(response[:data][:attributes][:first_name]).to eq('Pickles')
+    expect(response[:data][:attributes][:last_name]).to eq('McTickles')
+    expect(response[:data][:attributes][:email]).to eq('dilllord@dills.org')
+  end
 end
