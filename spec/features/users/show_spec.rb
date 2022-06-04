70e7f59c290190ec8b28db18734602af69b5e654
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-03 20:02:25 -0600

    Test: user show page exists

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "user show page" do
+  before do
+    visit "/users/1"
+  end
+
+  it "shows a specific user's data" do
+    expect(page).to have_content("Something this's Page")
+    expect(page).to have_content("Email: email@email.com")
+
+    expect(page).not_to have_content("asda this's Page")
+    expect(page).not_to have_content("Email: cheese@email.com")
+  end
+end
