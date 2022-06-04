56584a818af5fccbc45db7bf18c52b80a279af13
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-03 21:09:10 -0600

    Test: User Show displays 3 items from a user

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -12,4 +12,24 @@ describe "user show page" do
     expect(page).not_to have_content("asda this's Page")
     expect(page).not_to have_content("Email: cheese@email.com")
   end
+
+  it "shows a specific user's top 3 items", :vcr do
+    # note: due to little test data,
+    # this will only test for 2 items the user has.
+    within "#items" do
+      expect(page).to have_content("something this's Item Shed")
+
+      expect(page).to have_content("Water Bottle")
+      expect(page).to have_content("Count: 5")
+      expect(page).to have_content("Item ID: 1")
+
+      expect(page).to have_content("Trail Mix")
+      expect(page).to have_content("Count: 8")
+      expect(page).to have_content("Item ID: 2")
+
+      expect(page).not_to have_content("Good Socks")
+      expect(page).not_to have_content("Count: 3")
+      expect(page).not_to have_content("Item ID: 3")
+    end
+  end
 end
