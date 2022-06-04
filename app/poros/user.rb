8ff0ddcacb88fbc149668b4fed57e15587d0ae46
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-03 19:54:34 -0600

    Add first/last name to user poro

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,9 +1,10 @@
 class User
-  attr_reader :id,
-    :email
+  attr_reader :id, :first_name, :last_name, :email
 
   def initialize(data)
     @id = data[:id]
+    @first_name = data[:attributes][:first_name]
+    @last_name = data[:attributes][:last_name]
     @email = data[:attributes][:email]
   end
 end
