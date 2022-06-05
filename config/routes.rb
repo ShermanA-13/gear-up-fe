51b7ef6945726d45b31f8b1dfe6b1673ae5b05e0
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-05 15:48:35 -0600

    refactor ouath.config to allow for testing of login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'landing#index'
+  
   get '/auth/google_oauth2/callback', to: 'users#create'
   get '/dashboard', to: 'dashboard#show'
 
---
+++ spec/features/landing/index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Landing index page' do
+  before do
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+    visit root_path
+  end
+
+  it 'has button to login via Google', :vcr do
+    click_link 'Login With Google'
+
+    expect(current_path).to eq('/dashboard')
+  end
+end
