d76a2eca7c07052aac851d35be50eab53418d7bf
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-04 21:16:44 -0600

    Test: Item Edit page updates item when filled out

--- spec/features/items/edit_spec.rb
+++ spec/features/items/edit_spec.rb
@@ -15,4 +15,13 @@ describe "item edit page" do
 
     expect(find('form')).to have_button('Update Item')
   end
+
+  it "updates the item when form is filled out", :vcr do
+    fill_in "Description", with: "My Favorite Trail Mix"
+
+    click_button "Update Item"
+
+    expect(current_path).to eq("/users/1/items/2")
+    expect(page).to have_content("Description: My Favorite Trail Mix")
+  end
 end
