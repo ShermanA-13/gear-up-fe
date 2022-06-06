8793b27e4ab89990c56de28855ffc44fb6f5ef0f
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-06 17:24:48 -0600

    Test: area search page shows areas related to search

--- spec/features/areas/index_spec.rb
+++ spec/features/areas/index_spec.rb
@@ -8,4 +8,22 @@ describe "Area search (index) page" do
   it "has a search bar" do
     expect(find('form')).to have_content('Search')
   end
+
+  it "searches and returns some areas" do
+    fill_in "Search", with: "yosemite"
+    click_on "Search"
+
+    within("#area_1") do
+      expect(page).to have_content("Little Yosemite Valley")
+      expect(page).to have_content("California")
+    end
+    within("#area_2") do
+      expect(page).to have_content("Yosemite Point")
+      expect(page).to have_content("California")
+    end
+    within("#area_3") do
+      expect(page).to have_content("Yosemite Falls Wall")
+      expect(page).to have_content("California")
+    end
+  end
 end
