1b19ae920ee6532c83e5c32d2e4ec456749e201b
Alicia Watt <alicia.watt.aw@gmail.com>
2022-06-06 11:04:34 -0600

    update: trip attributes cassettes, trips show spec

--- spec/features/trips/show_spec.rb
+++ spec/features/trips/show_spec.rb
@@ -3,14 +3,13 @@ require "rails_helper"
 describe "Trip show page" do
   before do
     visit "/trips/1"
-    save_and_open_page
   end
 
-  it "displays the Trips's attributes (name, start_date, end_date)", :vcr do
-    expect(page).to have_content("Name: Adventure Time ")
-    expect(page).to have_content("Description: It's gonna be a wild one")
-    expect(page).to have_content("Start Date: 2022-08-14")
-    expect(page).to have_content("End Date: 2022-08-29")
+  it "displays the Trips attributes (name, start_date, end_date)", :vcr do
+    expect(page).to have_content("Name: first trip")
+    expect(page).to have_content("Start Date: 2022-06-04")
+    expect(page).to have_content("End Date: 2022-06-05")
+    expect(page).to have_content("Description: baby's first trip")
 
     expect(page).not_to have_content("Name: Big Bummer")
     expect(page).not_to have_content("Description: zero fun to be had")
