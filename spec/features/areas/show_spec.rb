4ec9f34ba1fd897e720c692cf1c5baf0540644c6
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-07 13:08:24 -0600

    Test: Area Show displays info for specific area

---
+++ spec/features/areas/show_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe "Area show page" do
+  before do
+    visit "/areas/2107"
+  end
+
+  it "shows specified area information" do
+    expect(page).to have_content("Little Yosemite Valley")
+    expect(page).to have_content("State: California")
+    expect(page).to have_content("Learn more about this route via The Mountain Project")
+    expect(page).to have_link("The Mountain Project")
+    within("#lat_lon_data") do
+      expect(page).to have_content("Latitude: 37.73648")
+      expect(page).to have_content("Longitude: -119.47885")
+    end
+
+    expect(page).not_to have_content("Yosemite Point")
+    expect(page).not_to have_content("Yosemite Falls Wall")
+    expect(page).not_to have_content("Yosemite Crack Zone (Left)")
+  end
+end
