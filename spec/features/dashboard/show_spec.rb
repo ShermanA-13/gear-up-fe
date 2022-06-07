f5230e24a0f6c4c05fb7641c9a0a9896c5c2c7d1
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-07 16:04:25 -0600

    Test: refactor dashboard specs to include user_photo attribute

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 describe 'creates user' do
   before :each do
     data = {
-      first_name: 'Pickles',
-      last_name: 'McTickles',
-      email: 'dilllord@dills.org'
+      first_name: 'Bonny',
+      last_name: 'Jowman',
+      email: 'ivebeentrapped@inthecomputer.org',
+      user_photo: 'https://lh3.googleusercontent.com/a-/AOh14GjhYI5RIF0qkDbiUtgXjH59K7hoEZ1QpLykFsEh2g=s96-c'
     }
 
     @user = UserFacade.create_user(data)
@@ -13,11 +14,11 @@ describe 'creates user' do
   end
 
   it 'displays user name', :vcr do
-    expect(page).to have_content('Pickles')
+    expect(page).to have_content('Bonny')
   end
 
   it 'has a link to the users item shed', :vcr do
-    click_link("My Shed")
+    click_link("MyShed")
     expect(current_path).to eq("/users/#{@user.id}/items")
   end
 end
