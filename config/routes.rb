ca97d2eff5806c205579ddd8766b582e8cadc3ec
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-07 12:35:00 -0600

    Add links to creat a trip on area search page

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,9 @@ Rails.application.routes.draw do
   end
 
   get "/areas/search", to: "areas#index"
+  # get "/areas/:area_id", to: "areas#show"
+  get "/areas/:area_id/trips/new", to: "trips#new"
+  # post "/areas/:area_id/trips/new", to: "trips#create"
+  # get "/areas/:area_id/trips/new/users", to: "user_trips#new"
+  # post "/areas/:area_id/trips/new/users", to: "user_trips#create"
 end
