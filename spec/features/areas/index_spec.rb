cadf92a41d83cad638b4e8d1620d85b5c7320c62
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-07 12:34:20 -0600

    Test: area index has links to create new trips

--- spec/features/areas/index_spec.rb
+++ spec/features/areas/index_spec.rb
@@ -9,21 +9,34 @@ describe "Area search (index) page" do
     expect(find('form')).to have_content('Search')
   end
 
-  it "searches and returns some areas", :vcr do
-    fill_in "Search", with: "yosemite"
-    click_on "Search"
+  describe "search functionality" do
 
-    within("#area_0") do
-      expect(page).to have_content("Little Yosemite Valley")
-      expect(page).to have_content("California")
+    before do
+      fill_in "Search", with: "yosemite"
+      click_on "Search"
     end
-    within("#area_1") do
-      expect(page).to have_content("Yosemite Point")
-      expect(page).to have_content("California")
+
+    it "searches and returns some areas", :vcr do
+      within("#area_0") do
+        expect(page).to have_content("Little Yosemite Valley")
+        expect(page).to have_content("California")
+      end
+      within("#area_1") do
+        expect(page).to have_content("Yosemite Point")
+        expect(page).to have_content("California")
+      end
+      within("#area_2") do
+        expect(page).to have_content("Yosemite Falls Wall")
+        expect(page).to have_content("California")
+      end
     end
-    within("#area_2") do
-      expect(page).to have_content("Yosemite Falls Wall")
-      expect(page).to have_content("California")
+
+    it "has a link to the new trip page", :vcr do
+      within("#area_0") do
+        click_on "Create a Trip for this Area"
+
+        expect(current_path).to eq("/areas/2107/trips/new")
+      end
     end
   end
 end
