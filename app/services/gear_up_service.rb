af03581e174ed23c322780595529f17b4aa4f42f
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-09 13:21:13 -0600

    comment out GearUp service and facade code - might delete entirely later

--- app/services/gear_up_service.rb
+++ app/services/gear_up_service.rb
@@ -1,6 +1,6 @@
-class GearUpService < BaseService
-  def self.user_trips(user_id)
-    response = conn.get("/api/v1/users/#{user_id}/trips")
-    get_json(response)
-  end
-end
+# class GearUpService < BaseService
+#   def self.user_trips(user_id)
+#     response = conn.get("/api/v1/users/#{user_id}/trips")
+#     get_json(response)
+#   end
+# end
