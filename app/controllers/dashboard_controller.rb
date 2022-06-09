16b10bd87f238b63577ed46e209d3b161ca61927
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-09 12:40:58 -0600

    Fix: refactor dashboard_controller to call TripFacade rather than GearUpFacade

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   def show
     @user = UserFacade.user(session[:user_id])
     @items = ItemFacade.items(session[:user_id])[0..2]
-    @trips = GearUpFacade.user_trips(session[:user_id]).sort_by{|trip| trip.start_date}[0..1]
+    @trips = TripFacade.trips_by_user_id(session[:user_id]).sort_by{|trip| trip.start_date}[0..1]
   end
 end
