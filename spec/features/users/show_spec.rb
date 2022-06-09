372b65ae5fc497ac01a8df634b882016b834e2b3
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-09 14:14:33 -0600

    refactor user_show spec to user webmock

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,69 +1,33 @@
 require "rails_helper"
 
-describe "user show page" do
+RSpec.describe "User show page" do
   before do
-    visit "/users/1"
+    @user = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
+    # @users = JSON.parse(File.read('spec/fixtures/users.json'), symbolize_names: true)
+    # @item = JSON.parse(File.read('spec/fixtures/item.json'), symbolize_names: true)
+    @items = JSON.parse(File.read('spec/fixtures/items.json'), symbolize_names: true)
+    @trips = JSON.parse(File.read('spec/fixtures/trips.json'), symbolize_names: true)
   end
 
-  it "shows a specific user's data", :vcr do
-    expect(page).to have_content("something this's Page")
-    expect(page).to have_content("Email: email@email.com")
-
-    expect(page).not_to have_content("asda this's Page")
-    expect(page).not_to have_content("Email: cheese@email.com")
-  end
-
-  it "shows a specific user's top 3 items", :vcr do
-    # note: due to little test data,
-    # this will only test for 2 items the user has.
-    within "#items" do
-      expect(page).to have_content("something this's Item Shed")
-
-      expect(page).to have_content("Water Bottle")
-      expect(page).to have_content("Count: 5")
-      expect(page).to have_content("Item ID: 1")
-
-      expect(page).to have_content("Trail Mix")
-      expect(page).to have_content("Count: 8")
-      expect(page).to have_content("Item ID: 2")
-
-      expect(page).not_to have_content("Good Socks")
-      expect(page).not_to have_content("Count: 3")
-      expect(page).not_to have_content("Item ID: 3")
+  describe "when logged in" do
+    before do
+      Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+      allow(UserService).to receive(:create_user).and_return(@user)
+      allow(UserService).to receive(:user).and_return(@user)
+      # allow(UserService).to receive(:users).and_return(@users)
+      allow(ItemService).to receive(:items).and_return(@items)
+      # allow(ItemService).to receive(:find_item).and_return(@item)
+      allow(TripService).to receive(:trips_by_user_id).and_return(@trips)
+      visit root_path
+      click_link 'Login'
     end
-  end
 
-  it "has links to item show pages", :vcr do
-    within "#items" do
-      click_link "Water Bottle"
-    end
-    expect(current_path).to eq("/users/1/items/1")
-    expect(page).to have_content("Name: Water Bottle")
-  end
+    it "shows user data" do
+      expect(page).to have_content("Bonny Jowman's Page")
+      expect(page).to have_content("Email: ivebeentrapped@inthecomputer.org")
 
-  it "has a link to the user shed", :vcr do
-    within "#items" do
-      click_link "something this's Shed"
-    end
-    expect(current_path).to eq("/users/1/items")
-    expect(page).to have_content("Water Bottle")
-    expect(page).to have_content("Trail Mix")
-  end
-
-  it "shows the next 2 upcoming trips the user is a part of", :vcr do
-      expect(page).to have_content("something this's Upcoming Trips")
-
-      within "#trip-1" do
-        expect(page).to have_content("first trip")
-        expect(page).to have_content("Description: baby's first trip")
-        expect(page).to have_content("Start date: 2022-06-06")
-        expect(page).to have_content("End date: 2022-06-07")
-      end
-      within "#trip-2" do
-        expect(page).to have_content("boo boo trip")
-        expect(page).to have_content("Description: trip I guess")
-        expect(page).to have_content("Start date: 2022-06-06")
-        expect(page).to have_content("End date: 2022-06-07")
+      expect(page).not_to have_content("monkey face's Page")
+      expect(page).not_to have_content("Email: foo@email.com")
     end
   end
 end
