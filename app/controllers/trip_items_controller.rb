df19f9ffc2ee9dbec817255d0e40c7cb8abc1b76
Susan Boyd <sueboyd922@gmail.com>
2022-06-08 20:17:25 -0400

    Feat: can add items to a trip

--- app/controllers/trip_items_controller.rb
+++ app/controllers/trip_items_controller.rb
@@ -3,11 +3,10 @@ class TripItemsController < ApplicationController
   def new
     @trip_id = params[:trip_id]
     @items = ItemFacade.items(params[:user_id])
-    # require "pry"; binding.pry
   end
 
   def create
-    # require "pry"; binding.pry
     TripFacade.add_items_to_trip(params[:trip_id], params[:items])
+    redirect_to "/trips/#{params[:trip_id]}"
   end
 end
