76acf6e6e7225656e2e6a642e9daed002fc14cc8
Sherman Au <shermau13@gmail.com>
2022-06-09 09:08:14 -0600

    refactor: FOR HEROKU!

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -3,16 +3,14 @@ class BaseService
     private
 
     def conn
+      if ENV['RAILS_ENV'] == 'test'
 
-      if ENV["RAILS_ENV"] == "test"
-
-        Faraday.new("http://localhost:5000")
+        Faraday.new('http://localhost:5000')
 
       else
-        # Faraday.new("https://gear-up-be.herokuapp.com")
-        Faraday.new("http://localhost:5000")
+        Faraday.new('https://gear-up-be.herokuapp.com')
+        # Faraday.new("http://localhost:5000")
       end
-
     end
 
     def get_json(response)
