44f2b4b187c5b3e24f575e435f2a0d89ee207380
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-09 11:20:08 -0600

    Test: refactor item_new 'creates a new item and redirects to item show page' spec

--- spec/features/items/new_spec.rb
+++ spec/features/items/new_spec.rb
@@ -4,19 +4,21 @@ RSpec.describe "Item new page" do
   before do
     Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
     user = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
+    item = JSON.parse(File.read('spec/fixtures/item.json'), symbolize_names: true)
     items = JSON.parse(File.read('spec/fixtures/items.json'), symbolize_names: true)
     trips = JSON.parse(File.read('spec/fixtures/trips.json'), symbolize_names: true)
     allow(UserService).to receive(:create_user).and_return(user)
     allow(UserService).to receive(:user).and_return(user)
     allow(ItemService).to receive(:items).and_return(items)
+    allow(ItemService).to receive(:create).and_return(item)
+    allow(ItemService).to receive(:find_item).and_return(item)
     allow(GearUpService).to receive(:user_trips).and_return(trips)
     visit root_path
     click_link 'Login'
+    visit "/users/3/items/new"
   end
 
   it "has a form to create a new item" do
-    visit "/users/3/items/new"
-
     expect(find('form')).to have_content('Item Name')
     expect(find('form')).to have_content('Description')
     expect(find('form')).to have_content('Count')
@@ -26,22 +28,22 @@ RSpec.describe "Item new page" do
     expect(find('form')).to have_button('Add Item')
   end
 
+  it "creates a new item and redirects to item show page" do
+    fill_in 'Item Name', with: "Harness"
+    fill_in 'Description', with: "Petzl Adjama"
+    fill_in 'Count', with: "1"
+    choose option: '6'
+    click_button 'Add Item'
+
+    expect(page).to have_content("Name: Harness")
+    expect(page).to have_content("Description: Petzl Adjama")
+    expect(page).to have_content("Count: 1")
+    expect(page).to have_content("Item Category: Harnesses")
+  end
+
   it "displays no form when user visits page" do
     visit "/users/2/items/new"
     expect(page).not_to have_css("form")
     expect(page).to have_content("How did you get here...")
-  end
-
-  it "creates a new item and redirects to item show page" do
-    fill_in 'Item Name', with: "Tent 1"
-    fill_in 'Description', with: "1 person Tent"
-    fill_in 'Count', with: "1"
-    choose option: '0'
-    click_button 'Add Item'
-
-    expect(page).to have_content("Name: Tent 1")
-    expect(page).to have_content("Description: 1 person Tent")
-    expect(page).to have_content("Count: 1")
-    expect(page).to have_content("Item Category: Tents")
   end
 end
