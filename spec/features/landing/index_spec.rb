0cf55cefc97c530e7022502b9dd5d124223e45b1
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-09 12:45:54 -0600

    refactor landing_index spec to use webmock

--- spec/features/landing/index_spec.rb
+++ spec/features/landing/index_spec.rb
@@ -3,10 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Landing index page' do
   before do
     Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+    @user = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
+    @items = JSON.parse(File.read('spec/fixtures/items.json'), symbolize_names: true)
+    @trips = JSON.parse(File.read('spec/fixtures/trips.json'), symbolize_names: true)
+    allow(UserService).to receive(:create_user).and_return(@user)
+    allow(UserService).to receive(:user).and_return(@user)
+    allow(ItemService).to receive(:items).and_return(@items)
+    allow(TripService).to receive(:trips_by_user_id).and_return(@trips)
     visit root_path
   end
 
-  it 'has button to login via Google', :vcr do
+  it 'has button to login via Google' do
     click_link 'Login'
     expect(current_path).to eq('/dashboard')
   end
