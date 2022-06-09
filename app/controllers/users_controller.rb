a01c0d3ba6c1c2d208d5f8db7834190a82b349ab
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-09 10:53:46 -0600

    Test: refactor item_new spec with webmock in before_do block

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     auth_hash = request.env['omniauth.auth']
      session[:access_token] = auth_hash[:credentials][:token]
     user = UserFacade.create_user(auth_hash[:info])
-
+    
     redirect_to "/login?user_id=#{user.id}&user_photo=#{user.user_photo}"
   end
 end
--- spec/features/items/new_spec.rb
+++ spec/features/items/new_spec.rb
@@ -1,37 +1,38 @@
 require "rails_helper"
 
-describe "Item new page" do
+RSpec.describe "Item new page" do
   before do
-    data = {
-      first_name: 'Bonny',
-      last_name: 'Jowman',
-      email: 'ivebeentrapped@inthecomputer.org',
-      user_photo: 'https://lh3.googleusercontent.com/a-/AOh14GjhYI5RIF0qkDbiUtgXjH59K7hoEZ1QpLykFsEh2g=s96-c'
-    }
-
-    @user = UserFacade.create_user(data)
-    visit "/login?user_id=#{@user.id}"
-    visit "/users/#{@user.id}/items/new"
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+    user = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
+    items = JSON.parse(File.read('spec/fixtures/items.json'), symbolize_names: true)
+    trips = JSON.parse(File.read('spec/fixtures/trips.json'), symbolize_names: true)
+    allow(UserService).to receive(:create_user).and_return(user)
+    allow(UserService).to receive(:user).and_return(user)
+    allow(ItemService).to receive(:items).and_return(items)
+    allow(GearUpService).to receive(:user_trips).and_return(trips)
+    visit root_path
+    click_link 'Login'
   end
 
-  it "has a form to create a new item", :vcr do
+  it "has a form to create a new item" do
+    visit "/users/3/items/new"
+
     expect(find('form')).to have_content('Item Name')
     expect(find('form')).to have_content('Description')
     expect(find('form')).to have_content('Count')
     expect(find('form')).to have_content('Tents')
     expect(find('form')).to have_content('Sleeping Bag')
     expect(find('form')).to have_content('Stoves, Grills & Fuel')
-
     expect(find('form')).to have_button('Add Item')
   end
 
-  it "displays no form when user visits page", :vcr do
+  it "displays no form when user visits page" do
     visit "/users/2/items/new"
     expect(page).not_to have_css("form")
     expect(page).to have_content("How did you get here...")
   end
 
-  it "creates a new item and redirects to item show page", :vcr do
+  it "creates a new item and redirects to item show page" do
     fill_in 'Item Name', with: "Tent 1"
     fill_in 'Description', with: "1 person Tent"
     fill_in 'Count', with: "1"
