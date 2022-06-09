d87e385ce1e5302faf01c3aa5583471fe8ce7e48
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-09 14:31:55 -0600

    remove unecessary UserService call from user_show webmock

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe "User show page" do
   before do
     @user = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
-    # @users = JSON.parse(File.read('spec/fixtures/users.json'), symbolize_names: true)
     @item = JSON.parse(File.read('spec/fixtures/user_item_show.json'), symbolize_names: true)
     @items = JSON.parse(File.read('spec/fixtures/items.json'), symbolize_names: true)
     @trips = JSON.parse(File.read('spec/fixtures/trips.json'), symbolize_names: true)
@@ -14,7 +13,6 @@ RSpec.describe "User show page" do
       Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
       allow(UserService).to receive(:create_user).and_return(@user)
       allow(UserService).to receive(:user).and_return(@user)
-      # allow(UserService).to receive(:users).and_return(@users)
       allow(ItemService).to receive(:items).and_return(@items)
       allow(ItemService).to receive(:find_item).and_return(@item)
       allow(TripService).to receive(:trips_by_user_id).and_return(@trips)
