d09e0349e7a880b890ce27ea4b87716826c8ac8b
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-09 11:53:06 -0600

    Test: refactor item_index 'displays all of the users items' with webmock and fixture data

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -1,54 +1,47 @@
 require "rails_helper"
 
-describe "Item index/shed" do
+RSpec.describe "Item index page" do
   before do
-    visit "/users/1/items"
+    @user = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
+    @item = JSON.parse(File.read('spec/fixtures/item.json'), symbolize_names: true)
+    @items = JSON.parse(File.read('spec/fixtures/items.json'), symbolize_names: true)
+    @trips = JSON.parse(File.read('spec/fixtures/trips.json'), symbolize_names: true)
   end
 
-  it "displays all of the users items", :vcr do
-    within "#item-1" do
-      expect(page).to have_content("Water Bottle")
-      expect(page).to have_content("Count: 5")
-      expect(page).to have_content("Item ID: 1")
-    end
-    within "#item-2" do
-      expect(page).to have_content("Trail Mix")
-      expect(page).to have_content("Count: 8")
-      expect(page).to have_content("Item ID: 2")
-    end
-
-    expect(page).not_to have_content("Good Socks")
-    expect(page).not_to have_content("Count: 3")
-    expect(page).not_to have_content("Item ID: 3")
-  end
-
-  it "each item links to the item's show page", :vcr do
-    within "#item-1" do
-      click_link("View Item")
+  describe "when logged in" do
+    before do
+      Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+      allow(UserService).to receive(:create_user).and_return(@user)
+      allow(UserService).to receive(:user).and_return(@user)
+      allow(ItemService).to receive(:items).and_return(@items)
+      allow(ItemService).to receive(:create).and_return(@item)
+      allow(ItemService).to receive(:find_item).and_return(@item)
+      allow(GearUpService).to receive(:user_trips).and_return(@trips)
+      visit root_path
+      click_link 'Login'
+      visit "/users/3/items"
     end
 
-    expect(current_path).to eq("/users/1/items/1")
-    expect(page).to have_content("Name: Water Bottle")
-    expect(page).to have_content("Count: 5")
-
-    expect(page).not_to have_content("Trail Mix")
-    expect(page).not_to have_content("Count: 8")
-  end
-end
-
-describe "create an item" do
-  before do
-    visit "/login?user_id=1"
-    click_link("My Shed")
-  end
-
-  it "has a button to create a new item", :vcr do
-    click_button("Add an item to your Shed")
-    expect(current_path).to eq("/users/1/items/new")
+    it "displays all of the users items" do
+      # save_and_open_page
+      within "#item-1" do
+        expect(page).to have_content("Tent 1")
+        expect(page).to have_content("Count: 1")
+        expect(page).to have_content("Item ID: 1")
+      end
+      within "#item-2" do
+        expect(page).to have_content("Organic Crash Pad")
+        expect(page).to have_content("Count: 5")
+        expect(page).to have_content("Item ID: 2")
+      end
+    end
   end
 
-  it "does not show the button when visiting a different users page", :vcr do
-    visit "/users/2/items"
-    expect(page).not_to have_button("Add an item to your Shed")
+  describe 'when not logged in' do
+    before do
+      allow(UserService).to receive(:user).and_return(@user)
+      allow(ItemService).to receive(:find_item).and_return(@item)
+      visit "/users/1/items/1"
+    end
   end
 end
