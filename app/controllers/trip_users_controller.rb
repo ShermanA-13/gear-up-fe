e1c3d1c4820e26eac9a28ac5fcf215a27592bd67
Alicia Watt <alicia.watt.aw@gmail.com>
2022-06-08 23:54:45 -0600

    rebase merge main to working branch

--- app/controllers/trip_users_controller.rb
+++ app/controllers/trip_users_controller.rb
@@ -1,6 +1,4 @@
 class TripUsersController < ApplicationController
-<<<<<<< HEAD
-<<<<<<< HEAD
   def new
     @trip = TripFacade.get_trip_by_id(params[:trip_id])
     @users = UserFacade.users
@@ -8,26 +6,12 @@ class TripUsersController < ApplicationController
 
   def create
     TripUsersFacade.add_trip_users(params[:user_ids], params[:trip_id])
-
     @trip = TripFacade.get_trip_by_id(params[:trip_id])
     redirect_to "/trips/#{@trip.id}"
   end
-=======
->>>>>>> e5d15df (feat: trip_users controller, services and routes)
-=======
-  # def index
-  # @users = TripUserFacade.
-  # end
-  #
-  # def create
-  #   @trip = trip.create(trip_params)
-  #    if @trip.save
-  #     TripUser.create(user_id: current_user.id, trip_id: @trip.id, is_host: true)
-  #     friend_ids.each { |id| TripUser.create(user_id: id, trip_id: @trip.id, is_host: false) } if friend_ids
-  #     redirect_to landing_path
-  #   else
-  #     redirect_to
-  #
-  # end
->>>>>>> f3a179c (rebase)
+
+  def update
+    TripUsersFacade.edit_invitees(params[:trip_id], params[:users])
+    redirect_to "/trips/#{@trip.id}"
+  end
 end
--- app/facades/trip_users_facade.rb
+++ app/facades/trip_users_facade.rb
@@ -2,4 +2,8 @@ class TripUsersFacade
   def self.add_trip_users(users, id)
     TripUsersService.create_trip_users(users, id)
   end
+
+  def self.edit_invitees(users, id)
+    TripUsersService.edit_invitees(users, id)
+  end
 end
