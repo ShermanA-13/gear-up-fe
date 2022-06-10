8cf945dfc300ca0864874a51c3c4b06b266a9d6c
Sherman Au <shermau13@gmail.com>
2022-06-09 22:51:56 -0600

    refactor: linter

--- app/services/trip_users_service.rb
+++ app/services/trip_users_service.rb
@@ -2,7 +2,7 @@ class TripUsersService < BaseService
   def self.create_trip_users(users, id)
     response = conn.post "/api/v1/trips/#{id}/users", {
       users: users
-    }.to_json, "Content-Type" => "application/json"
+    }.to_json, 'Content-Type' => 'application/json'
 
     get_json(response)
   end
@@ -16,7 +16,7 @@ class TripUsersService < BaseService
   def self.edit_invitees(user_ids, id)
     response = conn.patch "/api/v1/trips/#{id}/users", {
       users: user_ids
-    }.to_json, "Content-Type" => "application/json"
+    }.to_json, 'Content-Type' => 'application/json'
 
     get_json(response)
   end
