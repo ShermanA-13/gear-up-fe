36cae36c8e293680e87f0c2939bf0ad98e174997
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-09 19:29:31 -0600

    Trip Show Spec: All Tests Fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = UserFacade.user(params[:id])
-    
+
     if @user.class == User
       @items = ItemFacade.items(params[:id])[0..2]
-      @trips = GearUpFacade.user_trips(params[:id]).sort_by{|trip| trip.start_date}[0..1]
+      @trips = TripFacade.trips_by_user_id(session[:user_id]).sort_by{|trip| trip.start_date}[0..1]
     else
       @error = @user
     end
